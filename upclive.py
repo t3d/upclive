75660cd96f6a31f48c3cec93d6298a0d5366e5b1
Tomasz Długosz <tomek3d@gmail.com>
2011-11-08 22:31:38 +0100

    first commit

---
+++ upclive.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os
+
+vlc_opts = '--rtsp-tcp --rtsp-wmserver --rtsp-caching=5000 --clock-synchro=0'
+
+vlc_bin = 'vlc'
+
+channels = [
+  ('Animal Planet', 'rtsp://stream.livetv.chello.pl/Animal'),
+  ('Discovery', 'rtsp://stream.livetv.chello.pl/Discovery'),
+  ('CNN', 'rtsp://stream.livetv.chello.pl/CNN'),
+  ('TVN 24', 'rtsp://stream.livetv.chello.pl/TVN24'),
+  ('Mini Mini', 'rtsp://stream.livetv.chello.pl/MiniMini'),
+  ('teletoon+', 'rtsp://stream.livetv.chello.pl/ZigZap'),
+  ('National Geographic', 'rtsp://stream.livetv.chello.pl/NATGEO')
+  ]
+
+for channel_name, channel_url in channels:
+	print channel_name, channel_url
+	os.system( vlc_bin + ' ' + vlc_opts + ' ' + channel_url)
